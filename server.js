c3ba9119d3efc501790f8891f154118439e3ca08
Imran Jaleel <imranjaleel2331@gmail.com>
2024-03-13 21:52:20 +0530

    changes to port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 connectDB();
 
-const PORT = 3500;
+// const PORT = 3500;
 
 app.get("/", (req, res) => {
   res.send("hello ");
@@ -32,7 +32,7 @@ app.use("/api/wishlist", wishlistRouter);
 
 mongoose.connection.once("open", () => {
   console.log("connected to DB");
-  app.listen(process.env.PORT || PORT, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log("server is running");
   });
 });
