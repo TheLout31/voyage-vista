d13d38f988a059f06e6e61f1900d5919186f5e27
Imran Jaleel <imranjaleel2331@gmail.com>
2024-03-13 18:34:47 +0530

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require("cors")
 const hotelDataAddedToDBRouter = require("./routes/dataimport.router");
 const categoryDataAddedToDBRouter = require("./routes/categoryimport");
 
@@ -12,10 +12,11 @@ const wishlistRouter = require("./routes/wishlist.router");
 const connectDB = require("./config/dbconfig");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 connectDB();
 
-const PORT = 3000;
+const PORT = 3500;
 
 app.get("/", (req, res) => {
   res.send("hello ");
